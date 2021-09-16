5f1a16873c33b0403701fd9f5318bed4724d43a5
zacharywl123 <zsw23@nau.edu>
2021-09-15 18:08:15 -0700

    while doesnt work yet but making progress

--- Programming Assignment 2/server_C/client.c
+++ Programming Assignment 2/server_C/client.c
@@ -4,13 +4,12 @@
  * MAIN
  ************************************************************************/
 int main() {
-    char input[100];                    // buffer for user input
-    int client_socket;                  // client side socket
+    char input[100], buffer[100];       // buffer for user input
+    int client_socket, nread;                  // client side socket
     struct sockaddr_in client_address;  // client socket naming struct
     char c;
     
     printf("Daytime client\n");
-
 
     // create an unnamed socket, and then name it
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,8 +17,6 @@ int main() {
     client_address.sin_family = AF_INET;
     client_address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
     client_address.sin_port = htons(PORT);
-
-    printf("Made it here \n");
     
     // connect to server socket
     if (connect(client_socket, (struct sockaddr *)&client_address, sizeof(client_address)) == 0) {
@@ -30,22 +27,23 @@ int main() {
     
     while (TRUE) {
         printf("Enter IP for time: ");
+        
         // read string
         fgets(input, sizeof(input), stdin);
-        
-        int i = 0;
-        while (*(input + i)) {
-            // make the request to the server
-            write(client_socket, input + i, sizeof(char));
-            // get the result
-            read(client_socket, &c, sizeof(char));
-            if (c == 'q') {
-                close(client_socket);
-                printf("\nDone!\n");
-                exit(EXIT_SUCCESS);
+        // remove new line char from end of str  
+        input[strlen(input)-1] = '\0';
+
+        printf("inputed: %s\n", input);
+
+        printf("pre write\n");
+        while ((nread = read(client_socket, buffer, 100)) > 0) {
+            buffer[nread] = 0;
+            if (fputs(buffer, stdout) == EOF)
+            {
+                perror("fputs error"); /* write daytime on stdout */
+                return -1;
             }
-            printf("%c", c);
-            i++;
+            printf("buffer: %s\n", buffer);
         }
     }
     
--- Programming Assignment 2/server_C/server.h
+++ Programming Assignment 2/server_C/server.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 // socket/bind/listen/accept
 #include <arpa/inet.h>
@@ -31,9 +32,9 @@ void *handle_client(void *client_socket_void);
 /************************************************************************
  * preprocessor directives
  ************************************************************************/
-#define SERVER_ADDR "127.0.0.1" // loopback ip address
-// #define SERVER_ADDR "time.nist.gov" // loopback ip address
-#define PORT 8800             // port the server will listen on
+// #define SERVER_ADDR "127.0.0.1"  // loopback ip address
+#define SERVER_ADDR "time.nist.gov" // loopback ip address
+#define PORT 13                     // port the server will listen on
 
 // I cannot let go of the old-fashioned way :) - for readability ...
 #define FALSE false
