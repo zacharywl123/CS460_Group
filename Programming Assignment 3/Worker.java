9c3e2a7f2369917e0f4f9a313af879cac24af651
zacharywl123 <zsw23@nau.edu>
2021-09-29 17:34:09 -0700

    started

---
+++ Programming Assignment 3/Worker.java
@@ -0,0 +1,293 @@
+package web;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class Worker extends Thread implements HttpConstants {
+
+    final static int BUF_SIZE = 2048;
+    static final byte[] EOL = {(byte) '\r', (byte) '\n'};
+
+    /* buffer to use for requests */
+    byte[] buffer;
+    /* Socket to client we're handling */
+    private Socket socket;
+    /* WebServer the worker works for */
+    WebServer webServer;
+
+    Worker(WebServer webServer, String name) {
+        super(name);
+        this.webServer = webServer;
+        buffer = new byte[BUF_SIZE];
+        this.start();
+    }
+
+    synchronized void setSocket(Socket socket) {
+        this.socket = socket;
+        notify();
+    }
+
+    public synchronized void run() {
+
+        webServer.workerHasStarted();
+
+        while (true) {
+            /* nothing to do */
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                /* should not happen */
+                continue;
+            }
+
+            try {
+                handleClient();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            /* go back in wait queue if there's fewer
+             * than numHandler connections.
+             */
+
+            Vector pool = webServer.threads;
+            synchronized (pool) {
+                if (pool.size() >= webServer.workers) {
+                    /* too many threads, exit this one */
+                    return;
+                } else {
+                    pool.addElement(this);
+                }
+            }
+        }
+    }
+
+    void handleClient() throws IOException {
+        InputStream is = new BufferedInputStream(socket.getInputStream());
+        PrintStream ps = new PrintStream(socket.getOutputStream());
+        /* we will only block in read for this many milliseconds
+         * before we fail with java.io.InterruptedIOException,
+         * at which point we will abandon the connection.
+         */
+        socket.setSoTimeout(webServer.timeout);
+        socket.setTcpNoDelay(true);
+        /* zero out the buffer from last time */
+        for (int i = 0; i < BUF_SIZE; i++) {
+            buffer[i] = 0;
+        }
+        try {
+            /* We only support HTTP GET/HEAD, and don't
+             * support any fancy HTTP options,
+             * so we're only interested really in
+             * the first line.
+             */
+            int nread = 0, r = 0;
+
+            outerloop:
+            while (nread < BUF_SIZE) {
+                r = is.read(buffer, nread, BUF_SIZE - nread);
+                if (r == -1) {
+                    /* EOF */
+                    return;
+                }
+                int i = nread;
+                nread += r;
+                for (; i < nread; i++) {
+                    if (buffer[i] == (byte) '\n' || buffer[i] == (byte) '\r') {
+                        /* read one line */
+                        break outerloop;
+                    }
+                }
+            }
+
+            /* are we doing a GET or just a HEAD */
+            boolean doingGet;
+            /* beginning of file name */
+            int index;
+            if (buffer[0] == (byte) 'G'
+                    && buffer[1] == (byte) 'E'
+                    && buffer[2] == (byte) 'T'
+                    && buffer[3] == (byte) ' ') {
+                doingGet = true;
+                index = 4;
+            } else if (buffer[0] == (byte) 'H'
+                    && buffer[1] == (byte) 'E'
+                    && buffer[2] == (byte) 'A'
+                    && buffer[3] == (byte) 'D'
+                    && buffer[4] == (byte) ' ') {
+                doingGet = false;
+                index = 5;
+            } else {
+                /* we don't support this method */
+                ps.print("HTTP/1.0 " + HTTP_BAD_METHOD
+                        + " unsupported method type: ");
+                ps.write(buffer, 0, 5);
+                ps.write(EOL);
+                ps.flush();
+                socket.close();
+                return;
+            }
+
+            int i = 0;
+            /* find the file name, from:
+             * GET /foo/bar.html HTTP/1.0
+             * extract "/foo/bar.html"
+             */
+            for (i = index; i < nread; i++) {
+                if (buffer[i] == (byte) ' ') {
+                    break;
+                }
+            }
+            String fname = (new String(buffer, 0, index,
+                    i - index)).replace('/', File.separatorChar);
+            if (fname.startsWith(File.separator)) {
+                fname = fname.substring(1);
+            }
+
+            File targ = new File(webServer.root, fname);
+            webServer.print("" + targ);
+
+            if (targ.isDirectory()) {
+                File ind = new File(targ, "index.html");
+                if (ind.exists()) {
+                    targ = ind;
+                }
+            }
+            boolean OK = printHeaders(targ, ps);
+            if (doingGet) {
+                if (OK) {
+                    sendFile(targ, ps);
+                } else {
+                    send404(targ, ps);
+                }
+            }
+        } finally {
+            socket.close();
+        }
+    }
+
+    boolean printHeaders(File targ, PrintStream ps) throws IOException {
+        boolean ret = false;
+        int rCode = 0;
+        if (!targ.exists()) {
+            rCode = HTTP_NOT_FOUND;
+            ps.print("HTTP/1.0 " + HTTP_NOT_FOUND + " not found");
+            ps.write(EOL);
+            ret = false;
+        } else {
+            rCode = HTTP_OK;
+            ps.print("HTTP/1.0 " + HTTP_OK + " OK");
+            ps.write(EOL);
+            ret = true;
+        }
+        webServer.log("From " + socket.getInetAddress().getHostAddress() + ": GET "
+                + targ.getAbsolutePath() + "-->" + rCode);
+        ps.print("Server: Simple java");
+        ps.write(EOL);
+        ps.print("Date: " + (new Date()));
+        ps.write(EOL);
+        if (ret) {
+            if (!targ.isDirectory()) {
+                ps.print("Content-length: " + targ.length());
+                ps.write(EOL);
+                ps.print("Last Modified: " + (new Date(targ.lastModified())));
+                ps.write(EOL);
+                String name = targ.getName();
+                int ind = name.lastIndexOf('.');
+                String ct = null;
+                if (ind > 0) {
+                    ct = (String) map.get(name.substring(ind));
+                }
+                if (ct == null) {
+                    ct = "unknown/unknown";
+                }
+                ps.print("Content-type: " + ct);
+                ps.write(EOL);
+            } else {
+                ps.print("Content-type: text/html");
+                ps.write(EOL);
+            }
+        }
+        return ret;
+    }
+
+    void send404(File targ, PrintStream ps) throws IOException {
+        ps.write(EOL);
+        ps.write(EOL);
+        ps.println("Not Found<BR>"
+                + "The requested resource was not found.\n");
+    }
+
+    void sendFile(File targ, PrintStream ps) throws IOException {
+        InputStream is = null;
+        ps.write(EOL);
+        if (targ.isDirectory()) {
+            listDirectory(targ, ps);
+            return;
+        } else {
+            is = new FileInputStream(targ.getAbsolutePath());
+        }
+
+        try {
+            int n;
+            while ((n = is.read(buffer)) > 0) {
+                ps.write(buffer, 0, n);
+            }
+        } finally {
+            is.close();
+        }
+    }
+
+    /* mapping of file extensions to content-types */
+    static java.util.Hashtable map = new java.util.Hashtable();
+
+    static {
+        fillMap();
+    }
+
+    static void setSuffix(String k, String v) {
+        map.put(k, v);
+    }
+
+    static void fillMap() {
+        setSuffix("", "content/unknown");
+        setSuffix(".uu", "application/octet-stream");
+        setSuffix(".exe", "application/octet-stream");
+        setSuffix(".ps", "application/postscript");
+        setSuffix(".zip", "application/zip");
+        setSuffix(".sh", "application/x-shar");
+        setSuffix(".tar", "application/x-tar");
+        setSuffix(".snd", "audio/basic");
+        setSuffix(".au", "audio/basic");
+        setSuffix(".wav", "audio/x-wav");
+        setSuffix(".gif", "image/gif");
+        setSuffix(".jpg", "image/jpeg");
+        setSuffix(".jpeg", "image/jpeg");
+        setSuffix(".htm", "text/html");
+        setSuffix(".html", "text/html");
+        setSuffix(".text", "text/plain");
+        setSuffix(".c", "text/plain");
+        setSuffix(".cc", "text/plain");
+        setSuffix(".c++", "text/plain");
+        setSuffix(".h", "text/plain");
+        setSuffix(".pl", "text/plain");
+        setSuffix(".txt", "text/plain");
+        setSuffix(".java", "text/plain");
+    }
+
+    void listDirectory(File dir, PrintStream ps) throws IOException {
+        ps.println("<TITLE>Directory listing</TITLE><P>\n");
+        ps.println("<A HREF=\"..\">Parent Directory</A><BR>\n");
+        String[] list = dir.list();
+        for (int i = 0; list != null && i < list.length; i++) {
+            File f = new File(dir, list[i]);
+            if (f.isDirectory()) {
+                ps.println("<A HREF=\"" + list[i] + "/\">" + list[i] + "/</A><BR>");
+            } else {
+                ps.println("<A HREF=\"" + list[i] + "\">" + list[i] + "</A><BR");
+            }
+        }
+        ps.println("<P><HR><BR><I>" + (new Date()) + "</I>");
+    }
+}
