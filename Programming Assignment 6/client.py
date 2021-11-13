9d880781d3163c4dfaa4c25961453e20f5f88466
Zachary Wilson-Long <56366286+zacharywl123@users.noreply.github.com>
2021-11-12 18:44:36 -0700

    Mostly working

--- Programming Assignment 6/client.py
+++ Programming Assignment 6/client.py
@@ -1,13 +1,65 @@
-import threading
 import socket
-alias = input('Choose an alias : >>> ')
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.1.1"
+ADDR = (SERVER, PORT)
+ARITH_OPS = ['+', '-', '*', '/']
+
+
+# function that converts a math opertion in a string into a protocol string for the serer
+def operation_to_protocol(op_str):
+    output = 'Invalid'
+    input_lst = op_str.split()
+    possible_pwr = list(input_lst[0])
+
+    # if first and last args are numbers
+    if input_lst[0].isnumeric() and input_lst[2].isnumeric():
+        # if the middle item is a valid operator then
+        if input_lst[1] in ARITH_OPS:
+            output = input_lst[1] + '_' + input_lst[0] + '_' + input_lst[2]
+    
+    # if follows sqrt of format
+    elif input_lst[0] == 'sqrt' and input_lst[1] == 'of' and input_lst[2].isnumeric():
+        output = 'sqrt' + '_' + input_lst[2]
+
+    # if has a carrot symbol in argument
+    elif '^' in possible_pwr:
+        carrot_index = possible_pwr.index('^')
+        first_num = input_lst[0][0:carrot_index]
+        second_num = input_lst[0][carrot_index+1:]
+
+        #if both arguments on either side of carrot are numbers then valid
+        if first_num.isnumeric() and second_num.isnumeric():
+            output = '^' + '_' + first_num + '_' + second_num
+
+    return output
+
+
+output = 'Invalid'
+while output == 'Invalid':
+    operation = input('Enter in a valid Arithmetic Operation : >>> ')
+    output = operation_to_protocol(operation)
+    if output == 'Invalid':
+        print("Invalid Input Try Again...\n")
+    print(f"Protocol Form: '{output}'")
+print("VALID OPERATION!")
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
-def client_recieve():
-    while True:
-        try:
-            message = client_recv(1024).decode('utf-8')
-        except:
-            print('Error')
-            client.close()
-            break
+client.connect(ADDR)
+def client_recieve(message):
+    message = message.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+client_recieve(output)
+
+# op_for_server = '10'.encode(FORMAT)
+
+# client.send(op_for_server)
--- Programming Assignment 6/server.py
+++ Programming Assignment 6/server.py
@@ -1,36 +1,88 @@
 import threading
 import socket
+import math
 
-host = '127.0.0.1'
-port = 59000
+HEADER = 64
+PORT = 5050
+SERVER = "127.0.0.1"
+#SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ARITH_OPS = ['+', '-', '*', '/', '^', 'sqrt']
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind(ADDR)
+
 server.listen(5)
 clients = []
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+
+def conduct_operation(input_str):
+    output = 'Error'
+    input_list = input_str.split('_')
+    output_num = 0
+    
+    if input_list[0] in ARITH_OPS:
+        operator = input_list[0]
+        num_1 = int(input_list[1])
+
+        if operator == 'sqrt':
+            output_num = math.sqrt(num_1)
+        else:
+            num_2 = int(input_list[2])
+
+            if operator == '+':
+                output_num = num_1 + num_2
+
+            elif operator == '-':
+                output_num = num_1 - num_2
+            
+            elif operator == '*':
+                output_num = num_1 * num_2
+            
+            elif operator == '/':
+                output_num = num_1 - num_2
+            
+            elif operator == '^':
+                output_num = num_1 ** num_2
+        
+        output = str(output_num)
+
+    return output
+
 
 # Handles connections from clients 
-def handle_client(client):
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+
+        message_length = conn.recv(HEADER).decode(FORMAT)
+
+        if message_length:
+            message_length = int(message_length)
+            message = conn.recv(message_length).decode(FORMAT)
+
+            if message == 'DISCONNECT_MESSAGE':
+                connected = False
+            
+            print(f"[{addr}]  Input: {message}")
+            result = conduct_operation(message)
+            print(f"[{addr}] Result: {result}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove
-            client.close()
-            break
-# main function to recieve client connections
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'Connection is being established with {str(address)}')
-        client.send('You are now connected'.encode('utf-8'))
-        thread = threading.Thread(target = handle_client, args=(client,))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
 if __name__ == "__main__":
-    receive()
+    print ("[STARTING] server is starting ... ")
+    start()
